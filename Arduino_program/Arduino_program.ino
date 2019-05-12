613811dd589a6369a4422d39dfbc8254c98b0fda
EduardoLima100 <eduardolima100@gmail.com>
2019-05-12 14:10:25 -0300

    Teste de ativação do relé

---
+++ Arduino_program/Arduino_program.ino
@@ -0,0 +1,15 @@
+int RELE_1 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(RELE_1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(RELE_1, HIGH);
+  delay(1000);
+  digitalWrite(RELE_1, LOW);
+  delay(1000);
+}
